889fbeef5bbab51b146d5f77c935d5fcaab10246
romi <rv2574@bs.amalnet.k12.il>
2023-12-26 09:08:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/demo6/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.demo6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+ class MainActivity extends AppCompatActivity {
+    Button btn;
+    int x;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+    }
+
+    public void Clickme(View view) {
+        x++;
+        btn.setText(x);
+}}
+//
